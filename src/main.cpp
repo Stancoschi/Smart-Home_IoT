ae5eacf55b8ba61c5bddd32ce4470cc6fa0477b6
Eduard Robert Stancoschi <stancoschieduardrobert@gmail.com>
2024-03-01 16:43:20 +0200

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,129 @@
+#include <ESP8266WiFi.h>
+#include <DHT.h>
+#include <DHT_U.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+#include <PubSubClient.h>
+
+/************************* Pin Definition *********************************/
+
+// DHT11 for reading temperature and humidity value
+
+#define DHTPIN D3
+#define DHTTYPE DHT11 // DHT 11
+DHT dht(DHTPIN, DHTTYPE);
+String temp;
+String hum;
+
+// Your WiFi credentials.
+const char *ssid = "HUAWEI-2rPn";
+const char *password = "B9dfmugT";
+const char *mqtt_server = "192.168.100.4"; // this is our broker
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+unsigned long lastMsg = 0;
+#define MSG_BUFFER_SIZE (50)
+char msg[MSG_BUFFER_SIZE];
+int value = 0;
+int sensorValue = 0;
+
+void setup_wifi()
+{ // setting up wifi connection
+
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.mode(WIFI_STA);        // select "wifi station" mode (client mode)
+  WiFi.begin(ssid, password); // initiate connection
+
+  while (WiFi.status() != WL_CONNECTED)
+  { // wait for connection (retry)
+    delay(500);
+    Serial.print(".");
+  }
+
+  randomSeed(micros());
+
+  Serial.println(""); // when connected
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char *topic, byte *payload, unsigned int length)
+{ // callback funtion (trigger)
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++)
+  { // print received character array in one line
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  // Switch on the LED if an 1 was received as first character
+}
+
+void reconnect()
+{ // connecting to MQTT broker
+  // Loop until we're reconnected
+  while (!client.connected())
+  {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP8266Client-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect(clientId.c_str()))
+    {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish("LDR", "Hello I'm connected"); // run only once, just after mqtt connect
+      // ... and resubscribe
+      client.subscribe("LED");
+    }
+    else
+    {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void setup()
+{
+
+  // Debug console
+
+  Serial.begin(115200);
+  dht.begin();
+  setup_wifi();                        // call function
+  client.setServer(mqtt_server, 1883); // connect to MQTT
+  client.setCallback(callback);        // Listen to incoming messages and trigger callback
+}
+
+void loop()
+
+{
+  if (!client.connected())
+  { // retry until conncted
+    reconnect();
+  }
+  client.loop(); // MQTT loop, we keep listening
+  value = dht.readTemperature();
+  Serial.print(value);
+  Serial.print("Temp is: ");
+  char str[8];
+  itoa(value, str, 10); // convert int to char array
+  client.publish("esp8266/temp", str);
+  delay(2000); // loop 2second by 2second, you can change it, but don't flood our free broker
+}
